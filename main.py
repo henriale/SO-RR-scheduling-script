1c028e07be323f5aad73d4cd057c5ffd8a804eb9
gabrielkurtz <gfkurtz@gmail.com>
2018-05-05 17:13:46 -0300

    Ready Queue as List

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from time import sleep
+from collections import deque
 
 def main():
 	print(
@@ -29,35 +30,52 @@ def main():
 	C = 1
 	
 	processes = read_processes(file)
-	queue = []
 	file.close()
 
 	print("Time Quantum (TQ): %d" % TQ)
 	print("Context shift (C): %d" % C)
 	print("P  AT  BT  Priority")
 	
+	requests = []
 	for p in processes:
 		print("%d  %2d  %2d  %d" % (p.get_number(), p.get_arrival_time(), p.get_burst_time(), p.get_priority()))
+		requests.append(p)
 
-	T = 1
-	while True:
-		sleep(1.5)
-		print("Time: %d" % T)
+	ready = []
+	
+	time = 1
+	
+	running_process = None
+	running_delay = 0
+	context_counter = 0
 
-		# 1. verify if process start
-		for process in processes:
-			if process[0] == T:
-				# todo: sort by priority after appending
-				queue.append(process)
-
-		T = T + 1
-		# 2. verify if has process to run
-		if no_process_to_run():
+	while(time<35):
+		if(context_counter == True):
+			print("C")
+			context_counter += 1
 			continue
 
-		# 3. account context switching
-		T = T + 1
-		# 4. take from queue the process with highest priority to run
+		if(running_process):
+			print(running_process.get_number())
+		else:
+			print("-")
+	
+		for req in processes:
+			if(req.get_arrival_time() == time):
+				requests.remove(req)
+				ready.append(req)
+
+		if(ready):
+			ready.sort(key=lambda p: p.priority, reverse=False)
+			
+			if(not(running_process)):
+				running_process = ready[0]
+			if(ready[0].get_priority() < running_process.get_priority()):
+				running_process = ready[0]
+				context_counter = True
+
+		time += 1
+	
 
 
 class Process:
@@ -70,6 +88,10 @@ class Process:
 		self.burst_time = BT
 		# Priority
 		self.priority = P
+		# Answer Time
+		self.answer_time = 0
+		# Waiting Time
+		self.waiting_time = 0
 			
 	def get_number(self):
 		return self.number
@@ -103,6 +125,27 @@ def read_processes(file):
 
 def no_process_to_run():
 	return True
+
+	
+
+#	while True:
+#		sleep(1.5)
+#		print("Time: %d" % T)
+#
+#		# 1. verify if process start
+#		for process in processes:
+#			if process[0] == T:
+#				# todo: sort by priority after appending
+#				ready.append(process)
+#
+#		T = T + 1
+#		# 2. verify if has process to run
+#		if no_process_to_run():
+#			continue
+#
+#		# 3. account context switching
+#		T = T + 1
+#		# 4. take from ready the process with highest priority to run
 
 
 if __name__ == "__main__":
