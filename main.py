e2845d15c5ce465395aee505bb57084ee3b861af
gabrielkurtz <gfkurtz@gmail.com>
2018-05-07 01:19:42 -0300

    Printing Log as String. Improved Data Structure

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from time import sleep
 from collections import deque
 
+# Toggles if Data File will be printed before running algorithm
 _PRINT_DATA_FIRST_ = True
 
 def main():
@@ -22,51 +23,54 @@ def main():
 
 		''')
 
-	file = open("input.txt")
-
-	
 	# Reading input file and generating data structure
 	file = open("input.txt")
 	print("--- Importing Data ---")
+	# Total Processes
 	processes_count = int(file.readline())
-	TQ = int(file.readline())
-	C = 1
-	
-	processes = read_processes(file)
+	# Time Quantum Size
+	time_quantum = int(file.readline())
+	# Context Shift Size
+	context_shift_size = 1
+	# Holds every Process request read from File
+	requests = read_processes(file)
 	file.close()
 
-	requests = []
 
 	if(_PRINT_DATA_FIRST_):
-		print("Time Quantum (TQ): %d" % TQ)
-		print("Context shift (C): %d" % C)
+		print("Time Quantum: %d" % time_quantum)
+		print("Context shift: %d" % context_shift_size)
 		print("P  AT  BT  Priority")
 	
-		for p in processes:
+		for p in requests:
 			print("%d  %2d  %2d  %d" % (p.get_number(), p.get_arrival_time(), p.get_remaining_burst(), p.get_priority()))
-			requests.append(p)
 
-	else:
-		for p in processes:
-			requests.append(p)
 
+	# Keeps a copy of the original requests list (Will update Processes as they change)
 	original_requests = list(requests)
-	ready = []
-	queue = deque()
+	# Process being executed
 	running_process = None
-	context_shift = False
+	# Ready Queue (Processes that are ready to execute)
+	ready = []
+	# High Priority Queue (Ready processes with same priority as Running Process)
+	priority_queue = deque()
+	# Handles Context Shift (Will bypass Context Shift when Processor is Idle similarly to Moodle example)
+	context_shift_counter = 1
+	# Current time
 	time = 1
+	# Resulting String displaying Processes over Time
+	result = ""
 
 	print("\n--- Starting Processes ---")
 
 	# Will process one time unit as long as unfinished processes exist
-	while(requests or ready or queue or running_process):
+	while(requests or ready or priority_queue or running_process):
 		
 		#print("Request: " + str(requests[0].get_number()))
 		
-		#print(str(requests) + "\n" + str(ready) + "\n" + str(queue) + "\n" + str(running_process))
+		#print(str(requests) + "\n" + str(ready) + "\n" + str(priority_queue) + "\n" + str(running_process))
 
-		#print("Starting Loop - CS: " + str(context_shift) + " Time: " + str(time))
+		#print("Starting Loop - CS: " + str(context_shift_counter) + " Time: " + str(time))
 
 		# Checks if any requests have become ready
 		for req in requests:
@@ -75,74 +79,77 @@ def main():
 				ready.append(req)
 
 		# Checks if Context Shift is occurring
-		if(context_shift == True):
-			print("C")
-			context_shift = False
+		if(context_shift_counter < context_shift_size):
+			result += "C"
+			context_shift_counter += 1
 			time += 1
 			continue
 
 		# Handles Running Process
 		if(running_process):
 			running_process.execute(time)
-			print(running_process.get_number())
+			result += str(running_process.get_number())
 
 			# Finishes process if it is done
 			if(running_process.get_remaining_burst() == 0):
 				running_process.finish(time)
-				context_shift = True
+				context_shift_counter = 0
 			
-				if(queue):
-					running_process = queue.popleft()
+				if(priority_queue):
+					running_process = priority_queue.popleft()
 				else:
 					running_process = None
 			
 			# Swaps process if Time Quantum is reached
-			elif(running_process.get_quantum_counter() == 3):
+			elif(running_process.get_quantum_counter() == time_quantum):
 				running_process.reset_quantum_counter()
-				context_shift = True
+				context_shift_counter = 0
 			
-				if(queue):
-					queue.append(running_process)
-					running_process = queue.popleft()
+				if(priority_queue):
+					priority_queue.append(running_process)
+					running_process = priority_queue.popleft()
 
 		# Handles idle processor
 		else:
-			print("-")
+			result += "-"
 
-	
+		# Handles Ready Queue and High-Priority Queue
 		if(ready):
 			# Creates a copy of the ready list and sorts it by priority (maintains queue order at original Ready list)
 			sorted_ready = list(ready)
 			sorted_ready.sort(key=lambda p: p.priority, reverse=False)
 			
-			# Assigns a process to be run if processor is idle
+			# Assigns a process to be run if processor is idle (Does not trigger Context Shift accordingly with Moodle Example)
 			if(not(running_process)):
 				running_process = sorted_ready[0]
 				ready.remove(running_process)
-				context_shift == False
+				context_shift_counter == 1
 			
-			# Swaps processes if there is a process with higher priority than current process
+			# Swaps processes if there is a process with higher priority than current process, resets Context Shift
 			elif(sorted_ready[0].get_priority() < running_process.get_priority()):
 				new_priority = sorted_ready[0].get_priority()
 				
+				# Returns Running Process and High Priority Queue to Ready Queue
+				# Will maintain High Priority queue order and append Running Process last
+				for p in priority_queue:
+					ready.append(priority_queue.popleft())
 				ready.append(running_process)
-				for p in queue:
-					ready.append(queue.popleft())
 				
-				queue = deque()
+				# Creates new High Priority Queue with new highest priority and assigns Running Process
+				priority_queue = deque()
 				for p in ready:
 					if(p.get_priority() == new_priority):
-						queue.append(p)
+						priority_queue.append(p)
 						ready.remove(p)
 
-				running_process = queue.popleft()
-				context_shift = True
+				running_process = priority_queue.popleft()
+				context_shift_counter = 0
 
-			# If there are new processes with same priority as running process, adds them to queue
+			# If there are new processes with same priority as running process, adds them to priority_queue
 			elif(sorted_ready[0].get_priority() == running_process.get_priority()):
 				for p in ready:
 					if(p.get_priority() == running_process.get_priority()):
-						queue.append(p)
+						priority_queue.append(p)
 						ready.remove(p)
 						
 
@@ -150,13 +157,15 @@ def main():
 
 		
 #		for p in ready:
-#			print("CS: " + str(context_shift) + " Time: " + str(time) + " Ready: " + str(p.get_number()))
+#			print("CS: " + str(context_shift_counter) + " Time: " + str(time) + " Ready: " + str(p.get_number()))
 
-#		for p in queue:
+#		for p in priority_queue:
 #			print(str(time) + " Queue: " + str(p.get_number()))
 
 
 		time += 1
+	
+	print("\nProcessor Log:\n" + result)
 	
 
 
@@ -200,21 +209,23 @@ class Process:
 	def get_quantum_counter(self):
 		return self.quantum_counter
 
+	# Executes Process for one time unit
 	def execute(self, time):
 		self.remaining_burst -= 1
 		self.quantum_counter += 1
 		if(self.response_time == 0):
 			self.response_time = time - self.arrival_time
 	
+	# Resets Quantum Counter
 	def reset_quantum_counter(self):
 		self.quantum_counter = 0
 
+	# Finishes Process and calculates requested data
 	def finish(self, time):
 		self.turn_around_time = time - self.arrival_time
 		self.waiting_time = self.turn_around_time - self.burst_time
 
 		
-
 
 # Reads processes from file and returns them in a list of objects
 def read_processes(file):
@@ -235,30 +246,6 @@ def read_processes(file):
 		count += 1
 
 	return processes
-
-def no_process_to_run():
-	return True
-
-	
-
-#	while True:
-#		sleep(1.5)
-#		print("Time: %d" % T)
-#
-#		# 1. verify if process start
-#		for process in processes:
-#			if process[0] == T:
-#				# todo: sort by priority after appending
-#				ready.append(process)
-#
-#		T = T + 1
-#		# 2. verify if has process to run
-#		if no_process_to_run():
-#			continue
-#
-#		# 3. account context switching
-#		T = T + 1
-#		# 4. take from ready the process with highest priority to run
 
 
 if __name__ == "__main__":
