f375808c50433daef87f3e618b7d6f686cf150ff
Alexandre Araujo <alexandre.araujo@sap.com>
2018-05-07 20:40:16 -0300

    rename queues to uppercase and refactor process arrival block

--- main.py
+++ main.py
@@ -6,17 +6,17 @@ def main():
     [time_quantum, processes_count, context_shift_size, processes] = process.Reader('input.txt').read()
 
     # todo: use proper queue instead
-    requests = processes
+    INCOME_QUEUE = processes
     # Keeps a copy of the original requests list (Will update Processes as they change)
-    original_requests = list(requests)
+    original_requests = list(INCOME_QUEUE)
     # Process being executed
     running_process = None
     # Ready Queue (Processes that are ready to execute)
     # todo: use proper queue instead
-    ready = []
+    READY_QUEUE = []
     # todo: use proper queue instead
     # High Priority Queue (Ready processes with same priority as Running Process)
-    priority_queue = deque()
+    PRIORITY_QUEUE = deque()
     # Handles Context Shift (Will bypass Context Shift when Processor is Idle similar to Moodle example)
     context_shift_counter = 1
     # Current time
@@ -25,28 +25,19 @@ def main():
     result = ""
 
     # Will process one time unit as long as unfinished processes exist
-    while requests or ready or priority_queue or running_process:
+    while INCOME_QUEUE or READY_QUEUE or PRIORITY_QUEUE or running_process:
         # Checks if any requests have become ready
-        set_remove = []
-        for req in requests:
-            if req.get_arrival_time() == time:
+        for req in INCOME_QUEUE:
+            if not (req.get_arrival_time() == time):
+                continue
 
-                if running_process:
-                    # Adds new process to High Priority queue if it has same priority as current process
-                    if req.get_priority() == running_process.get_priority():
-                        priority_queue.append(req)
-                        set_remove.append(req)
-                    # Appends new process to Ready Queue if it has higher or lower priority (will be handled later)
-                    else:
-                        ready.append(req)
-                        set_remove.append(req)
-                # Appends new process to Ready Queue if there is no Running Process (will be handled later)
-                else:
-                    ready.append(req)
-                    set_remove.append(req)
+            if running_process and req.get_priority() == running_process.get_priority():
+                PRIORITY_QUEUE.append(req)
+            else:
+                READY_QUEUE.append(req)
 
-        while set_remove:
-            requests.remove(set_remove.pop())
+            INCOME_QUEUE.remove(req)
+
 
         # Checks if Context Shift is occurring
         if context_shift_counter < context_shift_size:
@@ -65,8 +56,8 @@ def main():
                 running_process.finish(time)
                 context_shift_counter = 0
 
-                if priority_queue:
-                    running_process = priority_queue.popleft()
+                if PRIORITY_QUEUE:
+                    running_process = PRIORITY_QUEUE.popleft()
                 else:
                     running_process = None
 
@@ -75,25 +66,25 @@ def main():
                 running_process.reset_quantum_counter()
                 context_shift_counter = 0
 
-                if priority_queue:
-                    priority_queue.append(running_process)
-                    running_process = priority_queue.popleft()
+                if PRIORITY_QUEUE:
+                    PRIORITY_QUEUE.append(running_process)
+                    running_process = PRIORITY_QUEUE.popleft()
 
         # Handles idle processor
         else:
             result += "-"
 
         # Handles Ready Queue and High-Priority Queue
-        if ready:
+        if READY_QUEUE:
             # Creates a copy of the ready list and sorts it by priority (maintains queue order at original Ready list)
-            sorted_ready = list(ready)
+            sorted_ready = list(READY_QUEUE)
             sorted_ready.sort(key=lambda p: p.priority, reverse=False)
 
             # Assigns a process to be run if processor is idle
             # (Does not trigger Context Shift accordingly with Moodle Example)
             if not running_process:
                 running_process = sorted_ready[0]
-                ready.remove(running_process)
+                READY_QUEUE.remove(running_process)
 
             # Swaps processes if there is a process with higher priority than current process, resets Context Shift
             elif sorted_ready[0].get_priority() < running_process.get_priority():
@@ -101,28 +92,28 @@ def main():
 
                 # Returns Running Process and High Priority Queue to Ready Queue
                 # Will maintain High Priority queue order and append Running Process last
-                for p in priority_queue:
-                    ready.append(priority_queue.popleft())
+                for p in PRIORITY_QUEUE:
+                    READY_QUEUE.append(PRIORITY_QUEUE.popleft())
 
-                ready.append(running_process)
+                READY_QUEUE.append(running_process)
 
                 # todo: if 2 or more arrives, should not remove element during iteration
                 # Creates new High Priority Queue with new highest priority and assigns Running Process
-                priority_queue = deque()
-                for p in ready:
+                PRIORITY_QUEUE = deque()
+                for p in READY_QUEUE:
                     if p.get_priority() == new_priority:
-                        priority_queue.append(p)
-                        ready.remove(p)
+                        PRIORITY_QUEUE.append(p)
+                        READY_QUEUE.remove(p)
 
-                running_process = priority_queue.popleft()
+                running_process = PRIORITY_QUEUE.popleft()
                 context_shift_counter = 0
 
             # If there are new processes with same priority as running process, adds them to priority_queue
             elif sorted_ready[0].get_priority() == running_process.get_priority():
-                for p in ready:
+                for p in READY_QUEUE:
                     if p.get_priority() == running_process.get_priority():
-                        priority_queue.append(p)
-                        ready.remove(p)
+                        PRIORITY_QUEUE.append(p)
+                        READY_QUEUE.remove(p)
 
         time += 1
 
