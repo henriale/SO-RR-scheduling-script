a99e0da83eea95163685ae2bebcbefb628b6bfe6
Alexandre Araujo <alexandre.araujo@sap.com>
2018-05-08 08:13:07 -0300

    create class Scheduler

--- main.py
+++ main.py
@@ -2,149 +2,159 @@ from collections import deque
 import process
 
 
-def main():
-    [time_quantum, processes_count, context_shift_size, processes] = process.Reader('input.txt').read()
+class Scheduler:
+    def __init__(self):
+        self.INCOME_QUEUE = []
+        self.READY_QUEUE = []
+        self.PRIORITY_QUEUE = deque()
+        self.time = 1
 
-    # todo: use proper queue instead
-    INCOME_QUEUE = processes
-    # Keeps a copy of the original requests list (Will update Processes as they change)
-    original_requests = list(INCOME_QUEUE)
-    # Process being executed
-    running_process = None
-    # Ready Queue (Processes that are ready to execute)
-    # todo: use proper queue instead
-    READY_QUEUE = []
-    # todo: use proper queue instead
-    # High Priority Queue (Ready processes with same priority as Running Process)
-    PRIORITY_QUEUE = deque()
-    # Handles Context Shift (Will bypass Context Shift when Processor is Idle similar to Moodle example)
-    context_shift_counter = 1
-    # Current time
-    time = 1
-    # Resulting String displaying Processes over Time
-    result = ""
+        # start
+        [self.time_quantum, self.processes_count, self.context_shift_size, self.processes] = process.Reader('input.txt').read()
 
-    # Will process one time unit as long as unfinished processes exist
-    while INCOME_QUEUE or READY_QUEUE or PRIORITY_QUEUE or running_process:
-        # Checks if any requests have become ready
-        for req in INCOME_QUEUE:
-            if not (req.get_arrival_time() == time):
+        # todo: use proper queue instead
+        self.INCOME_QUEUE = self.processes
+        # Keeps a copy of the original requests list (Will update Processes as they change)
+        self.original_requests = list(self.INCOME_QUEUE)
+        # Process being executed
+        self.running_process = None
+        # Ready Queue (Processes that are ready to execute)
+        # todo: use proper queue instead
+        self.READY_QUEUE = []
+        # todo: use proper queue instead
+        # High Priority Queue (Ready processes with same priority as Running Process)
+        self.PRIORITY_QUEUE = deque()
+        # Handles Context Shift (Will bypass Context Shift when Processor is Idle similar to Moodle example)
+        self.context_shift_counter = 1
+        # Current time
+        self.time = 1
+        # Resulting String displaying Processes over Time
+        self.result = ""
+
+    def clock(self):
+        self.time = self.time + 1
+
+    def run(self):
+        # Will process one time unit as long as unfinished processes exist
+        while self.INCOME_QUEUE or self.READY_QUEUE or self.PRIORITY_QUEUE or self.running_process:
+            # Checks if any requests have become ready
+            for req in self.INCOME_QUEUE:
+                if not (req.get_arrival_time() == self.time):
+                    continue
+
+                if self.running_process and req.get_priority() == self.running_process.get_priority():
+                    self.PRIORITY_QUEUE.append(req)
+                else:
+                    self.READY_QUEUE.append(req)
+
+                self.INCOME_QUEUE.remove(req)
+
+            # Checks if Context Shift is occurring
+            if self.context_shift_counter < self.context_shift_size:
+                self.result += "C"
+                self.context_shift_counter += 1
+                self.time += 1
                 continue
 
-            if running_process and req.get_priority() == running_process.get_priority():
-                PRIORITY_QUEUE.append(req)
+            # Handles Running Process
+            if self.running_process:
+                self.running_process.execute(self.time)
+                self.result += str(self.running_process.get_number())
+
+                # Finishes process if it is done
+                if self.running_process.get_remaining_burst() == 0:
+                    self.running_process.finish(self.time)
+                    self.context_shift_counter = 0
+
+                    if self.PRIORITY_QUEUE:
+                        self.running_process = self.PRIORITY_QUEUE.popleft()
+                    else:
+                        self.running_process = None
+
+                # Swaps process if Time Quantum is reached
+                elif self.running_process.get_quantum_counter() == self.time_quantum:
+                    self.running_process.reset_quantum_counter()
+                    self.context_shift_counter = 0
+
+                    if self.PRIORITY_QUEUE:
+                        self.PRIORITY_QUEUE.append(self.running_process)
+                        self.running_process = self.PRIORITY_QUEUE.popleft()
+
+            # Handles idle processor
             else:
-                READY_QUEUE.append(req)
+                self.result += "-"
 
-            INCOME_QUEUE.remove(req)
+            # Handles Ready Queue and High-Priority Queue
+            if self.READY_QUEUE:
+                # Creates a copy of the ready list and sorts it by priority (maintains queue order at original Ready list)
+                sorted_ready = list(self.READY_QUEUE)
+                sorted_ready.sort(key=lambda p: p.priority, reverse=False)
 
+                # Assigns a process to be run if processor is idle
+                # (Does not trigger Context Shift accordingly with Moodle Example)
+                if not self.running_process:
+                    self.running_process = sorted_ready[0]
+                    self.READY_QUEUE.remove(self.running_process)
 
-        # Checks if Context Shift is occurring
-        if context_shift_counter < context_shift_size:
-            result += "C"
-            context_shift_counter += 1
-            time += 1
-            continue
+                # Swaps processes if there is a process with higher priority than current process, resets Context Shift
+                elif sorted_ready[0].get_priority() < self.running_process.get_priority():
+                    new_priority = sorted_ready[0].get_priority()
 
-        # Handles Running Process
-        if running_process:
-            running_process.execute(time)
-            result += str(running_process.get_number())
+                    # Returns Running Process and High Priority Queue to Ready Queue
+                    # Will maintain High Priority queue order and append Running Process last
+                    for p in self.PRIORITY_QUEUE:
+                        self.READY_QUEUE.append(self.PRIORITY_QUEUE.popleft())
 
-            # Finishes process if it is done
-            if running_process.get_remaining_burst() == 0:
-                running_process.finish(time)
-                context_shift_counter = 0
+                    self.READY_QUEUE.append(self.running_process)
 
-                if PRIORITY_QUEUE:
-                    running_process = PRIORITY_QUEUE.popleft()
-                else:
-                    running_process = None
+                    # todo: if 2 or more arrives, should not remove element during iteration
+                    # Creates new High Priority Queue with new highest priority and assigns Running Process
+                    self.PRIORITY_QUEUE = deque()
+                    for p in self.READY_QUEUE:
+                        if p.get_priority() == new_priority:
+                            self.PRIORITY_QUEUE.append(p)
+                            self.READY_QUEUE.remove(p)
 
-            # Swaps process if Time Quantum is reached
-            elif running_process.get_quantum_counter() == time_quantum:
-                running_process.reset_quantum_counter()
-                context_shift_counter = 0
+                    self.running_process = self.PRIORITY_QUEUE.popleft()
+                    self.context_shift_counter = 0
 
-                if PRIORITY_QUEUE:
-                    PRIORITY_QUEUE.append(running_process)
-                    running_process = PRIORITY_QUEUE.popleft()
+                # If there are new processes with same priority as running process, adds them to priority_queue
+                elif sorted_ready[0].get_priority() == self.running_process.get_priority():
+                    for p in self.READY_QUEUE:
+                        if p.get_priority() == self.running_process.get_priority():
+                            self.PRIORITY_QUEUE.append(p)
+                            self.READY_QUEUE.remove(p)
 
-        # Handles idle processor
-        else:
-            result += "-"
+            self.time += 1
 
-        # Handles Ready Queue and High-Priority Queue
-        if READY_QUEUE:
-            # Creates a copy of the ready list and sorts it by priority (maintains queue order at original Ready list)
-            sorted_ready = list(READY_QUEUE)
-            sorted_ready.sort(key=lambda p: p.priority, reverse=False)
+        print("  P   AT   BT   Pri  CT  TAT   WT   RT ")
+        for p in self.original_requests:
+            print("%3d  %3d  %3d  %3d  %3d  %3d  %3d  %3d" % (
+                p.get_number(), p.get_arrival_time(), p.get_burst_time(), p.get_priority(),
+                p.get_completion_time(), p.get_turn_around_time(), p.get_waiting_time(), p.get_response_time()))
 
-            # Assigns a process to be run if processor is idle
-            # (Does not trigger Context Shift accordingly with Moodle Example)
-            if not running_process:
-                running_process = sorted_ready[0]
-                READY_QUEUE.remove(running_process)
+        print("\nProcessor Log:\n" + self.result)
 
-            # Swaps processes if there is a process with higher priority than current process, resets Context Shift
-            elif sorted_ready[0].get_priority() < running_process.get_priority():
-                new_priority = sorted_ready[0].get_priority()
+        # Calculating required averages
+        n = 0
+        total_response_time = 0
+        total_waiting_time = 0
+        total_turn_around_time = 0
 
-                # Returns Running Process and High Priority Queue to Ready Queue
-                # Will maintain High Priority queue order and append Running Process last
-                for p in PRIORITY_QUEUE:
-                    READY_QUEUE.append(PRIORITY_QUEUE.popleft())
+        for p in self.original_requests:
+            total_response_time += p.get_response_time()
+            total_waiting_time += p.get_waiting_time()
+            total_turn_around_time += p.get_turn_around_time()
+            n += 1
 
-                READY_QUEUE.append(running_process)
+        average_response_time = total_response_time / n
+        average_waiting_time = total_waiting_time / n
+        average_turn_around_time = total_turn_around_time / n
 
-                # todo: if 2 or more arrives, should not remove element during iteration
-                # Creates new High Priority Queue with new highest priority and assigns Running Process
-                PRIORITY_QUEUE = deque()
-                for p in READY_QUEUE:
-                    if p.get_priority() == new_priority:
-                        PRIORITY_QUEUE.append(p)
-                        READY_QUEUE.remove(p)
-
-                running_process = PRIORITY_QUEUE.popleft()
-                context_shift_counter = 0
-
-            # If there are new processes with same priority as running process, adds them to priority_queue
-            elif sorted_ready[0].get_priority() == running_process.get_priority():
-                for p in READY_QUEUE:
-                    if p.get_priority() == running_process.get_priority():
-                        PRIORITY_QUEUE.append(p)
-                        READY_QUEUE.remove(p)
-
-        time += 1
-
-    print("  P   AT   BT   Pri  CT  TAT   WT   RT ")
-    for p in original_requests:
-        print("%3d  %3d  %3d  %3d  %3d  %3d  %3d  %3d" % (
-            p.get_number(), p.get_arrival_time(), p.get_burst_time(), p.get_priority(),
-            p.get_completion_time(), p.get_turn_around_time(), p.get_waiting_time(), p.get_response_time()))
-
-    print("\nProcessor Log:\n" + result)
-
-    # Calculating required averages
-    n = 0
-    total_response_time = 0
-    total_waiting_time = 0
-    total_turn_around_time = 0
-
-    for p in original_requests:
-        total_response_time += p.get_response_time()
-        total_waiting_time += p.get_waiting_time()
-        total_turn_around_time += p.get_turn_around_time()
-        n += 1
-
-    average_response_time = total_response_time / n
-    average_waiting_time = total_waiting_time / n
-    average_turn_around_time = total_turn_around_time / n
-
-    print("\nAverage Response Time: " + str(average_response_time)
-          + "\nAverage Waiting Time: " + str(average_waiting_time)
-          + "\nAverage Turn Around Time: " + str(average_turn_around_time))
+        print("\nAverage Response Time: " + str(average_response_time)
+              + "\nAverage Waiting Time: " + str(average_waiting_time)
+              + "\nAverage Turn Around Time: " + str(average_turn_around_time))
 
 
 if __name__ == "__main__":
-    main()
+    Scheduler().run()
